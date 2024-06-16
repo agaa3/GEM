45aba0147a1eaf58b15c1fc5d057a3d4dfdabf31
JuliuszLuczak <235914@edu.p.lodz.pl>
2024-06-17 01:49:44 +0200

    RESPONSYWNOSC dla produktow i tyle w temacie

--- src/components/Product.js
+++ src/components/Product.js
@@ -154,22 +154,23 @@ export default function Product(props) {
 
     return (
         <div className="flex flex-col justify-center bg-beige p-4 ml-16">
-            <div className="flex items-center">
-                <div className='relative w-40 h-40 rounded-xl overflow-hidden'>
+            <div className="flex flex-col md:flex-row items-center">
+                <div className='relative w-40 h-40 rounded-xl overflow-hidden '>
                     <Image fill src={props.image} alt="" className='object-cover'/>
-                    <div className={`absolute top-28 right-0 px-0 py-0 text-green-b text-lg z-0 rounded-l-md w-[4.5rem] flex justify-center font-extrabold drop-shadow-[0_3px_6px_rgba(0,0,0,0.5)] ${
-                        props.category === 'Game1'
-                            ? 'bg-gra'
-                            : props.category === 'Game2'
+                    <div
+                        className={`absolute top-28 right-0 px-0 py-0 text-green-b text-lg z-0 rounded-l-md w-[4.5rem] flex justify-center font-extrabold drop-shadow-[0_3px_6px_rgba(0,0,0,0.5)] ${
+                            props.category === 'Game1'
                                 ? 'bg-gra'
-                                : props.category === 'Game3'
+                                : props.category === 'Game2'
                                     ? 'bg-gra'
-                                    : props.category === 'Album'
-                                        ? 'bg-album'
-                                        : props.category === 'E-Book'
-                                            ? 'bg-book'
-                                            : ''
-                    }`}>
+                                    : props.category === 'Game3'
+                                        ? 'bg-gra'
+                                        : props.category === 'Album'
+                                            ? 'bg-album'
+                                            : props.category === 'E-Book'
+                                                ? 'bg-book'
+                                                : ''
+                        }`}>
                         {props.category === 'Game1'
                             ? "Gra"
                             : props.category === 'Game2'
@@ -180,35 +181,37 @@ export default function Product(props) {
                     </div>
                 </div>
                 {/*<p className="py-2 ml-20 text-2xl text-green">{props.price}</p>*/}
-                <div className="flex flex-col justify-center p-4 w-[50%]">
+                <div className="flex flex-col justify-center p-4 md:w-[50%] md:p-2">
                     <p className="text-xl text-green-b">{props.author}</p>
                     <p className="text-xl text-green-b">{props.title}</p>
 
                     <div className='flex'>
                         <span className="text-green-b text-3xl">{props.price}</span>
-                        <img src="/photos/gem_symbol_dark.png" alt="gem" className="w-6 h-6 mt-1 ml-2" />
+                        <img src="/photos/gem_symbol_dark.png" alt="gem" className="w-6 h-6 mt-1 ml-2"/>
                     </div>
                 </div>
                 <div className="flex flex-col justify-center p-4">
                     {/*<Link href={} className="px-15 text-green">*/}
-                    {(props.edit ===undefined) && <button disabled={hasPurchased} onClick={handleClick}
-                             className="mt-4 bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3"
-                             style={{width: '200px'}}>
+                    {(props.edit === undefined) && <button disabled={hasPurchased} onClick={handleClick}
+                                                           className="mt-4 bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3"
+                                                           style={{width: '200px'}}>
                         Kup teraz!
                     </button>}
                     {props.edit === true && <button onClick={handleEdit}
-                                                     className="mt-4 bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3"
-                                                     style={{width: '200px'}}>
+                                                    className="mt-4 bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3"
+                                                    style={{width: '200px'}}>
                         Edytuj
-                    </button>}{props.edit === true && userInfo && userInfo.accountType === 3 && <button onClick={handleDelete}
-                                                               className="mt-4 bg-remove text-button px-4 py-2 rounded border border-#7F6E4D border-3"
-                                                               style={{width: '200px'}}>
-                    Usuń
+                    </button>}{props.edit === true && userInfo && userInfo.accountType === 3 &&
+                    <button onClick={handleDelete}
+                            className="mt-4 bg-remove text-button px-4 py-2 rounded border border-#7F6E4D border-3"
+                            style={{width: '200px'}}>
+                        Usuń
                     </button>}
 
                     {showChosenPopup && (
                         <div className="fixed z-40 top-0 left-0 w-full h-full flex items-center justify-center">
-                            <div className="bg-green text-beige text-centered h-64 w-[100%] px-4 py-2 rounded flex items-center justify-center" >
+                            <div
+                                className="bg-green text-beige text-centered h-64 w-[100%] px-4 py-2 rounded flex items-center justify-center">
                                 <ul>
                                     <li>
                                         <p className="text-center text-2xl">Czy na pewno chcesz kupić:</p>
@@ -217,15 +220,16 @@ export default function Product(props) {
                                         <p className="text-center mt-5 text-2xl">{props.title}, {props.author}?</p>
                                     </li>
                                     <li className="flex items-center justify-center">
-                                        <button onClick={handleConfirm} className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
+                                        <button onClick={handleConfirm}
+                                                className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
                                             Kupuję!
                                         </button>
-                                        <button onClick={handleDeny} className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
+                                        <button onClick={handleDeny}
+                                                className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
                                             Rezygnuję!
                                         </button>
                                     </li>
                                 </ul>
-
 
 
                             </div>
@@ -233,14 +237,17 @@ export default function Product(props) {
                     )}
                     {showPurchasedPopup && (
                         <div className="fixed z-50 top-0 left-0 w-full h-full flex items-center justify-center">
-                            <div className="bg-green text-beige text-centered h-64 w-[50%] px-4 py-2 rounded flex items-center justify-center" >
+                            <div
+                                className="bg-green text-beige text-centered h-64 w-[50%] px-4 py-2 rounded flex items-center justify-center">
                                 <ul className="w-[80%] full">
                                     <li><p className="text-center text-2xl">Zakupiono!</p></li>
                                     <li className="flex items-center justify-center">
-                                        <Link href="/comps/history" className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
+                                        <Link href="/comps/history"
+                                              className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
                                             Twoje produkty!
                                         </Link>
-                                        <button onClick={handleClose} className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
+                                        <button onClick={handleClose}
+                                                className="mt-4 w-[40%] mx-auto bg-button text-button px-4 py-2 rounded border border-#7F6E4D border-3">
                                             Wyjdź!
                                         </button>
                                     </li>
@@ -260,14 +267,16 @@ export default function Product(props) {
                     )}
                     {showFailedPopup && (
                         <div className="fixed z-50 top-0 left-0 w-full h-full flex items-center justify-center">
-                            <div className="bg-green text-beige text-centered h-64 w-[50%] px-4 py-2 rounded flex items-center justify-center" >
+                            <div
+                                className="bg-green text-beige text-centered h-64 w-[50%] px-4 py-2 rounded flex items-center justify-center">
                                 <p className="text-center text-2xl">Masz za mało kredytów!</p>
                             </div>
                         </div>
                     )}
                     {showNotLoggedPopup && (
                         <div className="fixed z-50 top-0 left-0 w-full h-full flex items-center justify-center">
-                            <div className="bg-green text-beige text-centered h-64 w-[50%] px-4 py-2 rounded flex items-center justify-center" >
+                            <div
+                                className="bg-green text-beige text-centered h-64 w-[50%] px-4 py-2 rounded flex items-center justify-center">
                                 <p className="text-center text-2xl">Aby wykupić subskrypcję musisz być zalogowany!</p>
                             </div>
                         </div>
